56ee705d8f2e641fa0d5b9d0eaf1f060f1509bfd
manishkushare <manishkushare@outlook.com>
2021-06-27 13:54:10 +0530

    learning absolute and relative path and how to use join method of path module to get absolute path

---
+++ block-BNaaax/server.js
@@ -0,0 +1,8 @@
+const path = require('path');
+console.log(__dirname," :dir name");
+console.log(__filename," :file name");
+
+const path_name = path.join(__dirname ,"server.js");
+
+console.log(path_name);
+
